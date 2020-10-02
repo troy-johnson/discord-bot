66ade6c2ed10d1aa10fe11b88c6bf417a44b838b
CNajm <33972857+CNajm@users.noreply.github.com>
2020-10-02 06:10:56 -0700

    Add anti-spam/verify command

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ dotenv.config();
 module.exports = {
 	discordToken: process.env.DISCORD_TOKEN,
 	googleApiKey: process.env.GOOGLE_API_KEY,
-	prefix: process.env.PREFIX
+	prefix: process.env.PREFIX,
+	generalRoleId: process.env.GENERAL_ROLE_ID
 };
